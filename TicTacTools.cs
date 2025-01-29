70de84acbfd4f9666d2bf3ae0377773f98cebf12
jbeales1 <153117269+jbeales1@users.noreply.github.com>
2025-01-29 09:41:29 -0700

    Update TicTacTools.cs

--- TicTacTools.cs
+++ TicTacTools.cs
@@ -3,13 +3,44 @@ namespace Mission4_Assignment;
 public class TicTacTools
 {
     // Recieve the board array from driver class
-    public static void PrintBoard(int[] boardArray)
+    public static void PrintBoard(char[] boardArray)
     {
         Console.WriteLine($"{boardArray[0]} | {boardArray[1]} | {boardArray[2]}");
         Console.WriteLine("--+---+--");
         Console.WriteLine($"{boardArray[3]} | {boardArray[4]} | {boardArray[5]}");
         Console.WriteLine("--+---+--");
         Console.WriteLine($"{boardArray[6]} | {boardArray[7]} | {boardArray[8]}");
+    }
+
+    public static int CheckForWinner(char[] boardArray)
+    {
+        List<int[]> winConditions = new List<int[]>
+        {
+            new int[] {0, 1, 2 }, new int[] {3, 4, 5 }, new int[] {6, 7, 8 },
+            new int[] {0, 3, 6 }, new int[] {1, 4, 7 }, new int[] {2, 5, 8 },
+            new int[] {0, 4, 8 }, new int[] {2, 4, 6 }
+        };
+
+        foreach (var condition in winConditions)
+        {
+            if (boardArray[condition[0]] != ' ' &&
+                    boardArray[condition[0]] == boardArray[condition[1]] &&
+                    boardArray[condition[1]] == boardArray[condition[2]])
+            {
+                if (boardArray[condition[0]] == 'x')
+                {
+                    return 1;
+                }
+                else if (boardArray[condition[0]] == 'o')
+                {
+                    return 2;
+                }
+            }
+
+        }
+
+        return 0;
+
     }
     
 }
