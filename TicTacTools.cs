d21a56f33938b5785a37d7cdc9c65a20cd13578a
Riley Wells <rwells9@byu.edu>
2025-01-29 09:02:28 -0700

    Added PrintBoard Method

--- TicTacTools.cs
+++ TicTacTools.cs
@@ -2,5 +2,15 @@ namespace Mission4_Assignment;
 
 public class TicTacTools
 {
+    // Recieve the board array from driver class
+    public static void PrintBoard(int[] boardArray)
+    {
+        Console.WriteLine($"{boardArray[0]} | {boardArray[1]} | {boardArray[2]}");
+        Console.WriteLine("--+---+--");
+        Console.WriteLine($"{boardArray[3]} | {boardArray[4]} | {boardArray[5]}");
+        Console.WriteLine("--+---+--");
+        Console.WriteLine($"{boardArray[6]} | {boardArray[7]} | {boardArray[8]}");
+    }
+
     
 }
